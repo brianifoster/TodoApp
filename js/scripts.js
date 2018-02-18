0830fd00923b914ae5571262c2f105d794dae727
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-17 19:43:32 -0800

    adds 'Done', 'Edit', and 'Delete' buttons with styling

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,18 @@ function addTodo(e) {
   var todoClear = document.querySelector('.todo-input').value = "";
   var list = document.querySelector('ul');
   var listItem = document.createElement('li');
+  var button1 = document.createElement('button');
+  var button2 = document.createElement('button');
+  var button3 = document.createElement('button');
 
   todos.push(todos);
   listItem.innerHTML = todoInput;
+  button1.innerHTML = "Done";
+  button2.innerHTML = "Edit";
+  button3.innerHTML = "Delete";
+  listItem.appendChild(button1);
+  listItem.appendChild(button2);
+  listItem.appendChild(button3);
   list.appendChild(listItem);
   console.log(todoInput);
 }
-
-console.log(todos);
