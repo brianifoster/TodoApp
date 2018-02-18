9e67784b3473c37ae7057844f032ccf57d4a8a85
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-17 18:23:12 -0800

    fine tunes html and gets input info

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-var todoInput = document.querySelector('.todo-input');
-var todoList = document.querySelector('.todo-list');
-var submit = document.querySelector('.submit');
+var newTodoForm = document.forms.todoForm;
+newTodoForm.addEventListener("submit", function(e) {
+  addTodo(e);
+});
+
+// submit.addEventListener('click', function() {
+//
+// })
+
+
+
+var todos = [];
+
+function addTodo(e) {
+  e.preventDefault();//stops it from actually submitting the information
+  let todoInput = document.querySelector('.todo-input').value;
+  let todoClear = document.querySelector('.todo-input').value = "";
+  todos.push(todos);
+  console.log(todoInput);
+}
