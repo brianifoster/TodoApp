26a4a4df39a15b3a75d975703dced6dd9b53c693
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-19 19:36:18 -0800

    tries to add a way to disable submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 //Function to have user add input and print it out with buttons
 var newTodoForm = document.forms.todoForm;
+
 newTodoForm.addEventListener("submit", function(e) {
   addTodo(e);
 });
 
-var todos = [];
+var stillTodos = [];
 
+/* This adds input from the user into a ul
+and then add a checkbox and a delete button */
 function addTodo(e) {
   e.preventDefault();
-  var ulStill = document.querySelector('.todo-list');
+  var ulStill = document.querySelector('.still-todo-list');
   var todoInput = document.querySelector('.todo-input').value;
   var todoClear = document.querySelector('.todo-input').value = "";
+
+  if(todoInput.value == null) {
+    document.querySelector('.submit').disabled = true;
+  } else {
+    document.querySelector('.submit').disabled = false;
   var li = document.createElement('li');
-  var h5Todo = document.createElement('h5');
-  h5Todo.className = 'header-four'
+  var h5Still = document.createElement('h5');
+  h5Still.className = 'header-four'
   var checkbox = document.createElement('input');
   checkbox.type = "checkbox";
   checkbox.id = "checkbox";
@@ -22,12 +30,13 @@ function addTodo(e) {
   deleteBtn.type = "button";
   deleteBtn.id = "delete-btn";
 
-  todos.push(todoInput);
+  stillTodos.push(todoInput);
   ulStill.appendChild(li);
-  li.appendChild(h5Todo);
+  li.appendChild(h5Still);
   li.appendChild(checkbox);
   li.appendChild(deleteBtn);
-  h5Todo.innerHTML = todoInput;
+  h5Still.innerHTML = todoInput;
   deleteBtn.innerHTML = "Delete";
   console.log(todoInput);
+  }
 }
