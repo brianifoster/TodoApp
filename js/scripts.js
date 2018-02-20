14ddb140a5c5056b9b98be0ec25b4d5a0cedc7dc
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-20 10:42:41 -0800

    disables submit button unless something is typed

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,15 @@ var newTodoForm = document.forms.todoForm;
 newTodoForm.addEventListener("submit", function(e) {
   addTodo(e);
 });
-
+// This adds a delete button and a checkbox to each list item
 var stillTodos = [];
 
-/* This adds input from the user into a ul
-and then add a checkbox and a delete button */
+// This adds input from the user into a ul
 function addTodo(e) {
   e.preventDefault();
   var ulStill = document.querySelector('.still-todo-list');
   var todoInput = document.querySelector('.todo-input').value;
   var todoClear = document.querySelector('.todo-input').value = "";
-
-  if(todoInput.length != "") {
-        document.querySelector('.submit').disabled = false;
-    } else {
-        document.querySelector('.submit').disabled = true;
-}
 
   var li = document.createElement('li');
   var labelStill = document.createElement('label');
@@ -31,9 +24,14 @@ function addTodo(e) {
   var deleteBtn = document.createElement('button');
   deleteBtn.type = "button";
   deleteBtn.id = "delete-btn";
+  if (todoInput === '') {
+    alert("You must write something!");
+  } else {
+    ulStill.appendChild(li);
+}
 
   stillTodos.push(todoInput);
-  ulStill.appendChild(li);
+
   li.appendChild(checkbox);
   li.appendChild(labelStill);
   li.appendChild(deleteBtn);
@@ -41,3 +39,8 @@ function addTodo(e) {
   deleteBtn.innerHTML = "Delete";
   console.log(todoInput);
 }
+
+// function boxChecked() {
+//   if(document.querySelector('#checkbox').checked = true)
+//
+// }
